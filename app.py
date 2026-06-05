8e24c9545ce853873661c54e3c95b9151602dd2b
Luci <matejmatej.cz@gmail.com>
2026-06-05 15:18:27 +0200

    Implement post deletion feature in ukaz_tema
    Added functionality to delete posts by index.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
 
@@ -13,13 +13,19 @@ def home():
 
 
 
-@app.route("/tema/<jazyk>", methods=["GET", "POST"])
+@app.route("/tema/<jazyk>", methods=["GET", "POST",])
 def ukaz_tema(jazyk):
     if request.method == "POST":
         napsany_text = request.form.get("prispevek")
         if napsany_text:
             prispevky.append(napsany_text)
+
+        elif request.form.get("smazani_index"):
+            prispevky.pop(int(request.form.get("smazani_index")))
+
         print(f"uživatel napsal do fóra {jazyk} tohle: {napsany_text}")
+
+
     
     return render_template("post.html", nazev=jazyk, prispevky=prispevky)
 
